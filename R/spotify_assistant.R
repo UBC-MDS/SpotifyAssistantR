6c94bc04f9fe17ecad781f129d4848e6fa848e74
Jenit Jain <jenitjai@student.ubc.ca>
2023-01-19 22:41:46 -0800

    Add docstring for get_users_top_genres()

--- R/spotify_assistant.R
+++ R/spotify_assistant.R
@@ -1,4 +1,6 @@
 library(spotifyr)
+library(tidyverse)
+library(dplyr)
 
 #' Get new releases by continent
 #'
@@ -10,10 +12,21 @@ library(spotifyr)
 #'
 #' @param country_cde A two digit string that
 #' @param n_limit Number of new releases to return
-#' @export
+#' @return List of new track releases in the given continent
 #' @examples
 #' get_new_releases_by_continent('Asia', 10)
 #' get_new_releases_by_continent('Europe', 15)
 get_new_releases_by_continent <- function(country_code, n_limit) {
+
+}
+
+#' Finds the top 5 genres from a user's saved tracks
+#'
+#' A wrapper function that goes through a user's entire saved library.
+#' For each song, it checks all the artist involved and the genre of music
+#' they compose. It returns the top 5 most commonly occurring genres.
+#'
+#' @return Character vector containing the top 5 genres of music in the User's saved library
+get_users_top_genres <- function() {
 
 }
