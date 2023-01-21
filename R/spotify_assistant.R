5486f0f51853d5e7af1015cdf349994e67874b6a
Caroline Tang <caroline.tang77@gmail.com>
2023-01-20 20:59:11 -0800

    added function doc

--- R/spotify_assistant.R
+++ R/spotify_assistant.R
@@ -30,3 +30,21 @@ get_new_releases_by_continent <- function(country_code, n_limit) {
 get_users_top_genres <- function() {
 
 }
+
+#' Get all the songs from a user's playlists
+#'
+#' A wrapper function that gets the names of all songs contained in one or more
+#' of a user's owned and saved playlists. By default, all songs from all playlists
+#' are returned, but playlists can also be specified by name.
+#'
+#' @param playlists a character vector containing the playlist names, defaults to all
+#' @return list containing a vector of song names from the playlists
+#' @export
+#'
+#' @examples
+#' get_playlists_songs()
+#' get_playlists_songs('night drives')
+#' get_playlists_songs(c('night drives', 'boss rush'))
+get_playlists_songs <- function(playlists = NULL) {
+
+}
