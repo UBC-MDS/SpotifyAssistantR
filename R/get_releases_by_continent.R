19e2824dcba7679f67c66d8c1147c3e3a1c8081d
Chester Wang <chesteraigo@dhcp-128-189-212-177.ubcsecure.wireless.ubc.ca>
2023-01-28 01:01:18 -0800

    Added func for getting new releases

--- R/get_releases_by_continent.R
+++ R/get_releases_by_continent.R
@@ -20,5 +20,41 @@ get_map <- function() {
 }
   
 
+#' Get all releases by continent
+#'
+#' @param continent continent name as a String
+#' @param limit number of releases to extract
+#' @return a list containing all info of selected new releases
+#' @importFrom spotifyr get_my_playlists
+get_releases <- function(continent, limit){
+  
+  cc_map <- get_map()
+  
+  if(limit <= 0){
+    stop("Limit parameter invalid")
+  }
+  
+  if(!continent %in% names(cc_map)){
+    stop("Continent parameter invalid. Check for typos")
+  }
+  
+  cat("-- Loading new releases in ", continent, ", this may take a while...", "\n")
+  
+  all_releases <- list()
+  
+  for(country_code in cc_map[[continent]]){
+    if(is_valid_code(country_code)) {
+      new_releases <- spotifyr::get_new_releases(country_code)
+      all_releases[[country_code]] <- new_releases
+    }
+    
+  }
+  
+  return(all_releases)
+}
+
+
+
+
 
 
